b55f556006f849c92d53bf7ade0b52bc94d11a60
Santos <santosphone22@gmail.com>
2025-05-22 12:15:35 -0600

    fix: ajustar redirección en el inicio de sesión y mejorar el texto en la página de inicio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,22 @@
+"use client";
+import { useEffect } from "react";
 import { AuthProvider } from "./context/AuthContext";
 import "./globals.css";
+import { useRouter } from "next/navigation";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const savedToken = localStorage.getItem("token");
+    const savedEmail = localStorage.getItem("email");
+
+    if (savedToken && savedEmail) {
+      router.push("/todos");
+    }
+
+  }, []);
+
   return (
     <AuthProvider>
       <html lang="es">
