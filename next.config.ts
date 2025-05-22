38f1add58b224da8ab95b0a0a8c59423b38a1f43
Santos <santosphone22@gmail.com>
2025-05-22 12:18:10 -0600

    fix: comentar la opción de salida para servir archivos estáticos en la configuración de Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  // output: 'export', // Solo para servir archivos 100% estáticos
   /*Configuraciones adicionales de variables de entorno*/
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
